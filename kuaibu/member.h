72346c7c73c3af5b3f3e9ba55679a9b39a013f25
yangtm <515872372@qq.com>
2015-08-20 14:01:01 +0800

    11

---
+++ kuaibu/member.h
@@ -0,0 +1,13 @@
+//
+//  member.h
+//  kuaibu
+//
+//  Created by yangtm on 15/8/20.
+//  Copyright (c) 2015年 yangtm. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface member : NSObject
+
+@end
---
+++ kuaibu/member.m
@@ -0,0 +1,13 @@
+//
+//  member.m
+//  kuaibu
+//
+//  Created by yangtm on 15/8/20.
+//  Copyright (c) 2015年 yangtm. All rights reserved.
+//
+
+#import "member.h"
+
+@implementation member
+
+@end
