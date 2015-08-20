06700c6411c82c3c75ea10f3be1d400a56abb7d2
yangtm <yangtm@yangtmdeMacBook-Pro.local>
2015-08-20 10:28:48 +0800

    Initial Commit

---
+++ kuaibu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  kuaibu
+//
+//  Created by yangtm on 15/8/20.
+//  Copyright (c) 2015年 yangtm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ kuaibu/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  kuaibu
+//
+//  Created by yangtm on 15/8/20.
+//  Copyright (c) 2015年 yangtm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
