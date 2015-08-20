f7b604ea83de5265cd864148cb21d6e3c6a93905
yangtm <515872372@qq.com>
2015-08-20 14:10:54 +0800

    11

---
+++ kuaibu/product.m
@@ -0,0 +1,13 @@
+//
+//  product.m
+//  kuaibu
+//
+//  Created by yangtm on 15/8/20.
+//  Copyright (c) 2015年 yangtm. All rights reserved.
+//
+
+#import "product.h"
+
+@implementation product
+
+@end
