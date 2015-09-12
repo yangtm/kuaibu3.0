0409a3503e77ab416cac8f78f34f0944ada752ac
孙琴琴 <sunqinqin@sunqinqindeMac-mini.local>
2015-09-11 18:56:15 -0700

    alice

---
+++ Class/HomePage/Cell/HomePageHotProductCell.h
@@ -0,0 +1,18 @@
+//
+//  HomePageHotProductCell.h
+//  kuaibu
+//
+//  Created by 孙琴琴 on 15/9/11.
+//  Copyright (c) 2015年 yangtm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomePageHotProductCell : UICollectionViewCell
+
+@property (strong, nonatomic) UIImageView *pruductImageView;
+@property (strong, nonatomic) UILabel *titleLabel;
+@property (strong, nonatomic) UILabel *priceLabel;
+@property (strong, nonatomic) UILabel *attention;
+@property (strong, nonatomic) UILabel *time;
+@end
